9863cd0dc4ef1856181d0d026f5512e37568c3a7
Ayman-BEDDA <bedda.ayman.sio@gmail.com>
2023-06-28 21:00:20 +0200

    test

--- pushAll.sh
+++ pushAll.sh
@@ -1,3 +1,4 @@
 (cd user-api && docker build -t mohammaddocker/tm-user-api . && docker push mohammaddocker/tm-user-api)
 (cd auth-api && docker build -t mohammaddocker/tm-auth-api . && docker push mohammaddocker/tm-auth-api)
 (cd task-api && docker build -t mohammaddocker/tm-task-api . && docker push mohammaddocker/tm-task-api)
+(cd user-api && docker build -t mohammaddocker/tm-recette-api . && docker push mohammaddocker/tm-user-api)
